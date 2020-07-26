b967c4900bff6d14fa0c7bc3cc7daf2306571a33
Sam Jordan <sam@netsells.co.uk>
2020-07-26 19:39:41 +0100

    Adds option to tunnel

--- app/Commands/AwsSsmConnect.php
+++ app/Commands/AwsSsmConnect.php
@@ -35,6 +35,12 @@ class AwsSsmConnect extends Command
     {
         $this->setDefinition(array_merge([
             new InputOption('instance-id', null, InputOption::VALUE_OPTIONAL, 'The instance ID to connect to'),
+            new InputOption('username', null, InputOption::VALUE_OPTIONAL, 'The username connect with'),
+            new InputOption('tunnel', null, InputOption::VALUE_NONE, 'Sets up an SSH tunnel'),
+            new InputOption('tunnel-remote-server', null, InputOption::VALUE_OPTIONAL, 'The SSH tunnel remote server'),
+            new InputOption('tunnel-remote-port', null, InputOption::VALUE_OPTIONAL, 'The SSH tunnel remote port'),
+            new InputOption('tunnel-local-port', null, InputOption::VALUE_OPTIONAL, 'The SSH tunnel local port'),
+            new InputOption('executable', null, InputOption::VALUE_NONE, 'Sets the output to the raw generated command'),
         ], $this->helpers->aws()->commonConsoleOptions()));
     }
 
@@ -44,6 +50,107 @@ class AwsSsmConnect extends Command
      * @return mixed
      */
     public function handle()
+    {
+        if ($this->option('executable') && (!$this->option('instance-id') || !$this->option('username'))) {
+            $this->line("echo 'You cannot use executable without specifying all required options'");
+            return;
+        }
+
+        if ($this->option('executable') && $this->option('tunnel') && (!$this->option('tunnel-remote-server') || !$this->option('tunnel-remote-port') || !$this->option('tunnel-local-port'))) {
+            $this->line("echo 'You cannot use executable without specifying all required tunnel options'");
+            return;
+        }
+
+        $rebuildOptions = [];
+
+        $instanceId = $this->option('instance-id') ?: $this->askForInstanceId();
+        $username = $this->option('username') ?: $this->askForUsername();
+
+        $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'username', $username);
+        $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'instance-id', $instanceId);
+
+        $awsOptions = implode(' ', [
+            '--aws-profile', $this->option('aws-profile') ?: 'default',
+            '--aws-region', $this->option('aws-region') ?: 'eu-west-2',
+        ]);
+
+        $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'aws-profile');
+        $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'aws-region');
+
+        $options = [
+            '-o', '"IdentityFile ~/.ssh/netsells-cli-ssm-ssh-tmp"',
+            '-o', '"IdentitiesOnly yes"',
+            '-o', '"GSSAPIAuthentication no"',
+            '-o', '"PasswordAuthentication no"',
+            '-o', "\"ProxyCommand bash -c \\\"$(php /Users/sam/code/desktop/netsells-cli/netsells aws:ssm:start-session {$username} {$instanceId} {$awsOptions})\\\"\"",
+        ];
+
+        if ($this->option('tunnel')) {
+            $tunnelRemoteServer = $this->option('tunnel-remote-server') ?: $this->askForTunnelRemoteServer();
+            $tunnelRemotePort = $this->option('tunnel-remote-port') ?: $this->askForTunnelRemotePort();
+            $tunnelLocalPort = $this->option('tunnel-local-port') ?: $this->askForTunnelLocalPort();
+
+            $options[] = '-N -L';
+            $options[] = sprintf('%s:%s:%s', $tunnelLocalPort, $tunnelRemoteServer, $tunnelRemotePort);
+
+            $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'tunnel-remote-server', $tunnelRemoteServer);
+            $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'tunnel-remote-port', $tunnelRemotePort);
+            $rebuildOptions = $this->appendResolvedArgument($rebuildOptions, 'tunnel-local-port', $tunnelLocalPort);
+        }
+
+        $command = sprintf("ssh %s %s@%s", implode(' ', $options), $username, $instanceId);
+
+        if ($this->option('executable')) {
+            $this->line($command);
+            return;
+        }
+
+        $this->info("Run the following command to connect:");
+        $this->info(' ');
+        $this->comment($command);
+        $this->info(' ');
+
+        $this->info("You can run this command again without having to go through options using this:");
+        $this->info(' ');
+        $this->comment("netsells aws:ssm:connect " . implode(' ', $rebuildOptions));
+        $this->info(' ');
+
+        // Can't do this yet
+        // $this->info("You can also return only the ssm connect string by passing in --executable");
+    }
+
+    protected function appendResolvedArgument($array, $key, $localValue = null): array
+    {
+        if ($this->option($key) || $localValue) {
+            $array[] = "--{$key}";
+            $array[] = $this->option($key) ?: $localValue;
+        }
+
+        return $array;
+    }
+
+    protected function askForTunnelRemoteServer()
+    {
+        return $this->ask("What is the remote server URL/IP for the SSH tunnel?");
+    }
+
+    protected function askForTunnelRemotePort()
+    {
+        return $this->ask("What is the remote port for the SSH tunnel?");
+    }
+
+    protected function askForTunnelLocalPort()
+    {
+        return $this->ask("What is the local port for the SSH tunnel?");
+    }
+
+    protected function askForUsername()
+    {
+        // TODO: Try and figure this out based on the AMI ID - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/connection-prereqs.html#connection-prereqs-get-info-about-instance
+        return $this->askWithCompletion("What user do you want to connect with?", ['ubuntu', 'ec2-user', 'admin', 'root']);
+    }
+
+    protected function askForInstanceId()
     {
         $instances = $this->helpers->aws()->ec2()->listInstances(
             $this,
@@ -58,29 +165,6 @@ class AwsSsmConnect extends Command
             return "[{$instance['InstanceId']}] {$instance['Name']} - {$instance['PrivateIpAddress']} {$instance['InstanceType']}";
         });
 
-        $instanceId = $this->menu("Choose an instance to connect to...", $instances->toArray())->open();
-
-        // TODO: Try and figure this out based on the AMI ID - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/connection-prereqs.html#connection-prereqs-get-info-about-instance
-        $username = $this->askWithCompletion("What user do you want to connect with?", ['ubuntu', 'ec2-user', 'admin', 'root']);
-
-        $awsOptions = implode(' ', [
-            '--aws-profile', $this->option('aws-profile') ?: 'default',
-            '--aws-region', $this->option('aws-region') ?: 'eu-west-2',
-        ]);
-
-        $options = [
-            '-o', '"IdentityFile ~/.ssh/netsells-cli-ssm-ssh-tmp"',
-            '-o', '"IdentitiesOnly yes"',
-            '-o', '"GSSAPIAuthentication no"',
-            '-o', '"PasswordAuthentication no"',
-            '-o', "\"ProxyCommand bash -c \\\"$(php /Users/sam/code/desktop/netsells-cli/netsells aws:ssm:start-session {$username} {$instanceId} {$awsOptions})\\\"\"",
-        ];
-
-        $command = sprintf("ssh %s %s@%s", implode(' ', $options), $username, $instanceId);
-
-        $this->info("Run the following command to connect:");
-        $this->info(' ');
-        $this->comment($command);
-        $this->info(' ');
+        return $this->menu("Choose an instance to connect to...", $instances->toArray())->open();
     }
 }
